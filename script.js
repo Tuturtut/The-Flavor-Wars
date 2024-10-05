9816dd94e52b8a4b803a450efe369e92b06209aa
Arthur <arthursimonin88@gmail.com>
2024-10-05 22:16:25 +0200

    U: some part of the site

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ items.forEach((item) => {
     item.classList.add("country-selected");
   });
 });
+
+const images = document.querySelectorAll(".countries-img");
+const titles = document.querySelectorAll(".country-title");
+
+titles.forEach((title, index) => {
+  title.addEventListener("mouseenter", () => {
+    const img = images[index];
+    const rect = img.getBoundingClientRect();
+    const overflowRight = rect.right - window.innerWidth;
+
+    if (overflowRight > 0) {
+      img.style.right = `calc(100vw - ${rect.width + rect.left}px)`; // Adjust the position
+    }
+  });
+});
